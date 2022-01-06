95259df0bd332ec0ec935bb8d178b9773510ceb5
fvilmot <fvilmot@gmail.com>
2022-01-06 17:43:38 +0100

    COUR-1 : Ajout du projet 12

---
+++ 12. Slider/app.js
@@ -0,0 +1,76 @@
+const imgs = document.querySelectorAll('.cont-slides img');
+const suivant = document.querySelector('.right');
+const precedent = document.querySelector('.left');
+const cercles = document.querySelectorAll('.cercle');
+let index = 0;
+
+
+function slideSuivante() {
+    if (index < 2) {
+        imgs[index].classList.remove('active');
+        index++;
+        imgs[index].classList.add('active');
+    } else if (index === 2) {
+        imgs[index].classList.remove('active');
+        index = 0;
+        imgs[index].classList.add('active');
+    }
+
+    for (i = 0; i < cercles.length; i++) {
+        if (cercles[i].getAttribute('data-clic') - 1 === index) {
+            cercles[i].classList.add('active-cercle');
+
+        } else {
+            cercles[i].classList.remove('active-cercle');
+        }
+    }
+}
+
+function slidePrecedente() {
+    if (index > 0) {
+        imgs[index].classList.remove('active');
+        index--;
+        imgs[index].classList.add('active');
+    } else if (index === 0) {
+        imgs[index].classList.remove('active');
+        index = 2;
+        imgs[index].classList.add('active');
+    }
+
+    for (i = 0; i < cercles.length; i++) {
+        if (cercles[i].getAttribute('data-clic') - 1 === index) {
+            cercles[i].classList.add('active-cercle');
+
+        } else {
+            cercles[i].classList.remove('active-cercle');
+        }
+    }
+}
+
+suivant.addEventListener('click', slideSuivante);
+precedent.addEventListener('click', slidePrecedente);
+
+document.addEventListener('keydown', keyPressed)
+
+function keyPressed(e) {
+    if (e.keyCode === 37) {
+        slidePrecedente();
+    }
+    else if (e.keyCode === 39) {
+        slideSuivante();
+    }
+}
+
+// Cercles
+cercles.forEach(cercle => {
+    cercle.addEventListener('click', function () {
+        for (i = 0; i < cercles.length; i++) {
+            cercles[i].classList.remove('active-cercle');
+        }
+        this.classList.add('active-cercle');
+
+        imgs[index].classList.remove('active');
+        index = this.getAttribute('data-clic') - 1;
+        imgs[index].classList.add('active');
+    })
+})
