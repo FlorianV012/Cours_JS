e1350360378668e4c647ed4da327347babb6a9da
fvilmot <fvilmot@gmail.com>
2022-01-31 14:19:14 +0100

    COUR-6 : Résolution algorithmes 1

--- algorithmes_01/01.Conversion/app.js
+++ algorithmes_01/01.Conversion/app.js
@@ -1,5 +1,13 @@
 function conversion(degCel) {
 
+    let degFar = (degCel * (9/5)) + 32;
+
+    if(typeof degCel === 'number'){
+        return degFar;
+    } else{
+        return "Données en entrée non correctes."
+    }
+
 
 }
 
