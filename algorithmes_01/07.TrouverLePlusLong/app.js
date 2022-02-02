9a423ef9dd06cf129d292e0e77cb32e3b07e2b72
fvilmot <fvilmot@gmail.com>
2022-02-02 10:30:18 +0100

    COUR-6 : Résolution algorithme 7

--- algorithmes_01/07.TrouverLePlusLong/app.js
+++ algorithmes_01/07.TrouverLePlusLong/app.js
@@ -1,11 +1,47 @@
-function trouverLongueurMax(str){
+// Boucle For
+function trouverLongueurMax(str) {
+    const mots = str.split(' ');
+    let longueurMax = 0;
 
+    for (i = 0; i < mots.length; i++) {
+        if (mots[i].length > longueurMax) {
+            longueurMax = mots[i].length;
+        }
+    }
 
+    return longueurMax;
 }
-
 
 console.log(trouverLongueurMax("Du sublime au ridicule il n'y a qu'un pas."));
 
+
+// Méthode Callback
+function trouverLongueurMaxCallback(str) {
+    const mots = str.split(' ');
+
+    return mots.reduce((x, y) => {
+        return Math.max(x, y.length)
+    }, 0)
+}
+
+console.log(trouverLongueurMaxCallback("Du sublime au ridicule il n'y a qu'un pas."));
+
+
+// Méthode Callback
+function trouverLongueurMaxCRecursion(str) {
+    const mots = str.split(' ');
+
+    if (mots.length === 1) {
+        return mots[0].length;
+    } else if (mots[0].length >= mots[1].length) {
+        mots.splice(1, 1);
+        return trouverLongueurMaxCRecursion(mots.join(' '));
+    } else if (mots[0].length <= mots[1].length) {
+        return trouverLongueurMaxCRecursion(mots.slice(1).join(' '));
+    }
+}
+
+console.log(trouverLongueurMaxCRecursion("Du sublime au ridicule il n'y a qu'un pas."));
 
 
 // ----------------- ÉNONCÉ -----------------
@@ -16,7 +52,7 @@ console.log(trouverLongueurMax("Du sublime au ridicule il n'y a qu'un pas."));
 
 // Le but ici va être de le faire de trois manières différentes, avec trois concepts
 // différents.
- 
+
 // 1: avec une boucle For.
 // 2: avec une méthode Callback.(reduce,Math.max)
 // 3: avec une récursion.(slice, splice)
