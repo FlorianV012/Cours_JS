b2cb5ee4ca5558d0d8fc99ba37b16f62f7a37c59
fvilmot <fvilmot@gmail.com>
2022-02-04 10:18:34 +0100

    COUR-6 : Résolution algorithme 14

--- algorithmes_01/14.trouveEtRemplace/app.js
+++ algorithmes_01/14.trouveEtRemplace/app.js
@@ -1,8 +1,16 @@
 function fonctionParam(arr, func) {
+    const array1 = arr.sort();
 
+    for (i = 0; i < array1.length; i++) {
+        if (func(array1[0])) {
+            return array1;
+        } else {
+            array1.shift();
+        }
+    }
 }
 
-console.log(fonctionParam([1, 7, 2, 3, 90, 4], function(n) {return n >= 3;}));
+console.log(fonctionParam([1, 7, 2, 3, 90, 4], function (n) { return n >= 3; }));
 
 
 // ----------------- ÉNONCÉ -----------------
