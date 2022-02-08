a1ee287130810f0f113503e7fff17d6511ffafec
fvilmot <fvilmot@gmail.com>
2022-02-08 15:29:09 +0100

    COUR-6 : Résolution algorithme 16

--- algorithmes_01/16.MeilleurInvestissement/app.js
+++ algorithmes_01/16.MeilleurInvestissement/app.js
@@ -1,16 +1,36 @@
-function meilleurInvestissement(arr){
+function meilleurInvestissement(arr) {
+    let meilleurProfit = 0;
+    let test = 0;
+    let pos = 0;
+    let date = [];
+    const jours = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"];
 
+    for (i = 0; i < arr.length; i++) {
+        test = arr[i];
 
+        for (j = pos; j < arr.length; j++) {
+            if (arr[j] - test > meilleurProfit) {
+                meilleurProfit = arr[j] - test;
+
+                date.push([test, arr[j]]);
+                if (date.length > 1) {
+                    date.shift();
+                }
+            }
+        }        
+        pos++;
+    }
+    console.log(date);
+    return `Le meilleur coup à faire cette semaine était d'investir le ${jours[arr.indexOf(date[0][0])]} et de revendre le ${jours[arr.indexOf(date[0][1])]} pour un bénéfice de ${meilleurProfit}.`;
 }
 
-console.log(meilleurInvestissement([50,10,20,2,80,60,20]));
-
+console.log(meilleurInvestissement([50, 10, 20, 2, 80, 60, 20]));
 
 // ----------------- ÉNONCÉ -----------------
 
-// Vous avez 7 valeurs dans un tableau qui représente le cours de l'or pour les 
+// Vous avez 7 valeurs dans un tableau qui représente le cours de l'or pour les
 // 7 jours d'une semaine.
-// Vous devez, grace à un algorithme, retourner le meilleur jour pour investir et 
+// Vous devez, grace à un algorithme, retourner le meilleur jour pour investir et
 // le meilleur jour pour vendre.
 // Ici le meilleur jour pour investir est Jeudi(4eme valeur du tableau), avec un cours
 // à 2€ et le meilleur pour vendre est vendredi, avec un cours à 80 donc un bénéfice de 78€.
