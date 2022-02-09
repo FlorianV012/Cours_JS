2c98f37f7a4b215ec2f94637cfb072f04fc66c07
fvilmot <fvilmot@gmail.com>
2022-02-09 11:53:36 +0100

    COUR-6 : Résolution algorithme 18

--- algorithmes_01/18.TabAplati/app.js
+++ algorithmes_01/18.TabAplati/app.js
@@ -1,9 +1,22 @@
 function aplatisCeTableau(arr) {
 
+    let tableauFinal = [];
 
+    function aplatir(arg) {
+        if (Array.isArray(arg)) {
+            for (i = 0; i < arr.length; i++) {
+                aplatir(arg[i]);
+            }
+        } else {
+            tableauFinal.push(arg)
+        }
+    }
+    arr.forEach(aplatir);
+
+    return tableauFinal;
 }
-  
-console.log(aplatisCeTableau([1, {}, [3, [[4]]]] ));
+
+console.log(aplatisCeTableau([1, {}, [3, [[4]]]]));
 
 // [1, [2], [3, [[4]]]]  retourne [1,2,3,4];
 // ([1, [], [3, [[4]]]]) retourne [1, 3, 4].
@@ -11,7 +24,7 @@ console.log(aplatisCeTableau([1, {}, [3, [[4]]]] ));
 
 // ----------------- ÉNONCÉ -----------------
 
-// Cet algorithme prends un tableau composé lui même de plusieurs tableaux en 
+// Cet algorithme prends un tableau composé lui même de plusieurs tableaux en
 // entrée. Vous devez "aplatir" les différentes couches de tableaux, c'est à dire
 // retourner un seul tableau qui contient les valeurs de tous les tableaux qui le
 // composent.
