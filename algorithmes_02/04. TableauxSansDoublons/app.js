0d6166cc4bedc515e4afab910161f21c30c7febd
fvilmot <fvilmot@gmail.com>
2022-02-15 14:31:44 +0100

    COUR-6 : Résolution algorithme partie 2-04

--- algorithmes_02/04. TableauxSansDoublons/app.js
+++ algorithmes_02/04. TableauxSansDoublons/app.js
@@ -1,12 +1,32 @@
+/* function tableauxSansDoublons() {
+    let nvTableau = [];
+
+    const arr = Array.from(arguments);
+
+    arr.forEach(el => {
+        for (i = 0; i < el.length; i++) {
+            nvTableau.push(el[i]);
+        }
+    })
+
+    const tableauTrie = nvTableau.filter((item, index) => nvTableau.indexOf(item) === index);
+    
+    return tableauTrie;
+} */
+
 function tableauxSansDoublons(...arrays) {
+    let nvTableau = [];
 
+    arrays.forEach(item => {
+        nvTableau = [...nvTableau, ...item]
+    })
 
-
+    return Array.from(new Set(nvTableau));
 }
 
 
-console.log(tableauxSansDoublons([5,5,4], [6,3,5])); // [5, 4, 6, 3]
-console.log(tableauxSansDoublons(["a","b","z"],["z","e","a"])); // ["a", "b", "z", "e"]
+console.log(tableauxSansDoublons([5, 5, 4], [6, 3, 5])); // [5, 4, 6, 3]
+console.log(tableauxSansDoublons(["a", "b", "z"], ["z", "e", "a"])); // ["a", "b", "z", "e"]
 
 
 // -----------------  Énoncé ------------------- //
