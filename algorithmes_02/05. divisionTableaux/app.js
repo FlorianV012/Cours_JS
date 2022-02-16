32e0b3421b4c4a79f040a96905ccba33a5a78f58
fvilmot <fvilmot@gmail.com>
2022-02-16 14:23:14 +0100

    COUR-6 : Résolution algorithme partie 2-05

--- algorithmes_02/05. divisionTableaux/app.js
+++ algorithmes_02/05. divisionTableaux/app.js
@@ -1,13 +1,31 @@
+/* function diviseTableau(tab, tailleMorceau) {
+    let tableauFinal = [];
+
+    for (let i = 0; i < tab.length; i += tailleMorceau) {
+        let morceau = tab.slice(i, i + tailleMorceau);
+        tableauFinal.push(morceau);
+    }
+
+    return tableauFinal;
+} */
+
 function diviseTableau(tab, tailleMorceau) {
+    let tableauFinal = [];
 
+    let copieTab = [...tab];
 
+    while (copieTab.length > 0) {
+        tableauFinal.push(copieTab.splice(0, tailleMorceau))
+    }
+
+    return tableauFinal;
 }
 
 
 
 
-console.log(diviseTableau([4,2,1,3,5,4,7,8,9,8], 3)); // [[4, 2, 1],[3, 5, 4],[7, 8, 9],[8]]
-console.log(diviseTableau(["e","r","r","m","z","e","a"], 5)); // [["e", "r", "r", "m", "z"],["e", "a"]]
+console.log(diviseTableau([4, 2, 1, 3, 5, 4, 7, 8, 9, 8], 3)); // [[4, 2, 1],[3, 5, 4],[7, 8, 9],[8]]
+console.log(diviseTableau(["e", "r", "r", "m", "z", "e", "a"], 5)); // [["e", "r", "r", "m", "z"],["e", "a"]]
 
 
 // -----------------  Énoncé ------------------- //
