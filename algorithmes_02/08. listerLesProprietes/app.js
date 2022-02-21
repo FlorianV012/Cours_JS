b8881b285fa7daf9573892c3ecfad148c0e56264
fvilmot <fvilmot@gmail.com>
2022-02-21 15:21:55 +0100

    COUR-6 : Résolution algorithme partie 2-08

--- algorithmes_02/08. listerLesProprietes/app.js
+++ algorithmes_02/08. listerLesProprietes/app.js
@@ -1,10 +1,24 @@
-function tailleDeLobj(obj){
+function tailleDeLobj(obj) {
+    let longueur = Object.keys(obj).length;
 
-
+    return longueur
 }
 
-console.log(tailleDeLobj({nom: 'Julia', taille: 168, age: 35})); //  
- 
+/* function tailleDeLobj(obj) {
+    let longueur = 0;
+
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            longueur++;
+        }
+    }
+
+    return longueur
+} */
+
+
+console.log(tailleDeLobj({ nom: 'Julia', taille: 168, age: 35 })); //  
+
 
 
 
